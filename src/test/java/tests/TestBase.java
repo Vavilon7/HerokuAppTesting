b162ab6060b51a04fc9809b170384e50a91f5a94
Tatiana <vicoltatiana4@gmail.com>
2025-04-06 09:13:41 +0200

    Added ApplicationManager class

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,27 +1,29 @@
 package tests;
 
+import com.theinternet.pages.config.ApplicationManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.time.Duration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TestBase {
+    protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", "chrome"));
     protected WebDriver driver;
-
+    public static Logger logger = LoggerFactory.getLogger(TestBase.class);
     @BeforeEach
-    public void setUp() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://the-internet.herokuapp.com");
+    //@BeforeAll
+    public void init(TestInfo testInfo) {
+        driver= app.startTest();
+        logger.info("Start test -> " + testInfo.getTestMethod().toString());
     }
 
     @AfterEach
     public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
+        app.stopTest();
+        logger.info("Stop test");
+        logger.info("**************************************************************");
+
     }
 }
